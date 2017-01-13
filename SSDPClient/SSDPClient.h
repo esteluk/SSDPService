8b2e73b71d23909ed8b981c5d5bd6d9d3e16f4b8
Nathan Wong <nathan@nathanwong.co.uk>
2017-01-13 00:28:35 +0000

    Initial Commit

---
+++ SSDPClient/SSDPClient.h
@@ -0,0 +1,19 @@
+//
+//  SSDPClient.h
+//  SSDPClient
+//
+//  Created by Nathan Wong on 13/01/2017.
+//  Copyright © 2017 Nathan Wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SSDPClient.
+FOUNDATION_EXPORT double SSDPClientVersionNumber;
+
+//! Project version string for SSDPClient.
+FOUNDATION_EXPORT const unsigned char SSDPClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSDPClient/PublicHeader.h>
+
+
